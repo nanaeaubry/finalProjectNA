85a1faf0ddd27ea4dbc32c40e91368da4830fd88
nanaeaubry <nanaeaubry@gmail.com>
2017-08-14 20:28:27 -0700

    added JS

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-$("#location_input").keypress(function(e) {
-  if (e.which == 13) {
-    console.log($(this).val());
-  }
+$(function() {
+  $("#location_input").keypress(function(e) {
+    if (e.which == 13) {
+      console.log($(this).val());
+    }
+  });
 });
