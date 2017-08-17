929808dac2e2a5c1472fd23e1742161084e0f45f
nanaeaubry <nanaeaubry@gmail.com>
2017-08-16 21:01:52 -0700

    map code

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
 $(function() {
   $("#location_input").keypress(function(e) {
     if (e.which == 13) {
-      console.log($(this).val());
+      // Implement a search to happen on map
+      $(this).val('');
     }
   });
+});
+
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/widgets/Locate",
+  "esri/widgets/Search",
+  "dojo/domReady!"
+], function(
+  Map, MapView, Locate, Search
+) {
+  var map = new Map({
+    basemap: "streets",
+    ground: "world-elevation"
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-56.049, 38.485, 78],
+    zoom: 3
+
+  });
+
+  var locateBtn = new Locate({
+    view: view
+  });
+
+  // Add the locate widget to the top left corner of the view
+  view.ui.add(locateBtn, {
+    position: "top-left"
+  });
+
+  var search = new Search({
+    view: view,
+    sources: $("#location_input").val()
+  });
+
 });
